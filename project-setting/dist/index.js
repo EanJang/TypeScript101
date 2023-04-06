0d35b2e5dedd5b5e00d1bb2eb6ddd61994e23ee2
LauJang <klaude.jang@gmail.com>
2023-04-06 14:59:27 +0900

    npm run dev/npm run build

---
+++ project-setting/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const example_1 = require("./utils/example");
+(0, example_1.testMakePerson)();
+//# sourceMappingURL=index.js.map
