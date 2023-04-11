aa35453f2c9a44af09d6c455b35f2d3c6ee811be
LauJang <klaude.jang@gmail.com>
2023-04-11 10:11:02 +0900

    Update import path

--- imperative-declarative/src/declarative-sum.ts
+++ imperative-declarative/src/declarative-sum.ts
@@ -1,5 +1,5 @@
-import { range } from "./range";
-import { fold } from "./fold";
+import { range } from "./utils/range";
+import { fold } from "./utils/fold";
 
 let numbers: number[] = range(1, 100 + 1)
 let result = fold(numbers, (result, value) => result + value, 0)
