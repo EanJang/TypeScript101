cc85555c7c12e6219bff8d235528680363cca727
LauJang <klaude.jang@gmail.com>
2023-04-11 10:09:13 +0900

    imperative vs declarative: sum of odd numbers 1 to 100

--- imperative-declarative/src/declarative-sum.ts
+++ imperative-declarative/src/declarative-sum.ts
@@ -3,4 +3,4 @@ import { fold } from "./fold";
 
 let numbers: number[] = range(1, 100 + 1)
 let result = fold(numbers, (result, value) => result + value, 0)
-console.log(result); //5050
+console.log(result); // 5050
