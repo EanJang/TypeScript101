b7a67a601646533cc1d70aa22c79e9fb6ab786e7
LauJang <klaude.jang@gmail.com>
2023-04-12 09:46:16 +0900

    imperative vs declarative: sum of square 1 to 100

---
+++ imperative-declarative/src/declarative-square-sum.ts
@@ -0,0 +1,10 @@
+import { range } from "./utils/range";
+import { fold } from "./utils/fold";
+import { map } from "./utils/map";
+
+let numbers: number[] = range(1, 100 + 1)
+let result = fold(
+    map(numbers, value => value * value),
+    (result, value) => result + value, 0)
+
+console.log(result); // 338350
