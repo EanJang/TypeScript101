f9c9b5951c7a7e7a142511161778f151ec626af5
LauJang <klaude.jang@gmail.com>
2023-04-17 17:15:28 +0900

    filter method example_1: filter odd and even number from array

---
+++ method-chain/src/filter/filter-odd-even.ts
@@ -0,0 +1,8 @@
+const range = (from: number, to: number): number[] =>
+    from < to ? [from, ...range(from + 1, to)] : []
+
+const array: number[] = range(1, 10+1)
+
+let odds: number[] = array.filter((value) => value % 2 != 0)
+let evens: number[] = array.filter((value) => value % 2 == 0)
+console.log(odds, evens);
