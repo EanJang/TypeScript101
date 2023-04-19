8b344b9404149a092b8c8896ac8a3d553c5f7e8d
LauJang <klaude.jang@gmail.com>
2023-04-19 18:32:30 +0900

    Fix typo

--- method-chain/src/map/map.ts
+++ method-chain/src/map/map.ts
@@ -1,2 +1,2 @@
-// When the type of an array is T[], filter method for array follows the structure below
+// When the type of an array is T[], map method for array follows the structure below
 // map(callback: (value: T, index?: number): Q): Q[]
