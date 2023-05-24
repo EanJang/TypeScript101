011acaf5f94e5b5fb57bf09d6bec2d29648a0649
EanJang <sooyean.jang@gmail.com>
2023-05-24 10:06:52 +0900

    Break down the generator

--- iterator/src/generator/yield/yield-star.ts
+++ iterator/src/generator/yield/yield-star.ts
@@ -11,3 +11,12 @@ function* generate12345() {
 
 // Test generators!
 for (let value of generate12345()) console.log(value); //1 2 3 4 5
+
+// yield:
+//  In the `generate12()` function, `yield` is used to product the values 1 and 2.
+//  When yield is encountered, the generator pauses and emits the specified value to the caller.
+
+// yield*:
+//  In the `generate12345()` function, `yield*` is used to delegate the iteration control to other iterable expressions.
+//  `yield* generate12()` delegates the iteration control to the `generate12()` function, which yields the values 1 and 2.
+//  `yield* [3, 4]` delegates the iteration control to the array `[3, 4]`, which is also an iterable object.
