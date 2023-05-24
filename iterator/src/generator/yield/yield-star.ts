422d78e7fc352bfdc1d5f6173249811c73793fb0
EanJang <sooyean.jang@gmail.com>
2023-05-24 09:40:17 +0900

    Test generator

--- iterator/src/generator/yield/yield-star.ts
+++ iterator/src/generator/yield/yield-star.ts
@@ -3,8 +3,11 @@ function* generate12() {
   yield 2;
 }
 
-function* generate345() {
+function* generate12345() {
   yield* generate12();
   yield* [3, 4];
   yield 5;
 }
+
+// Test generators!
+for (let value of generate12345()) console.log(value); //1 2 3 4 5
